dd3168b639761cd15823a9b5b8c6973011094a84
Not Defterim User <user@example.com>
2026-01-29 12:58:46 +0300

    Fix mobile horizontal overflow v22

--- script.js
+++ script.js
@@ -731,7 +731,7 @@ document.addEventListener('DOMContentLoaded', () => {
         document.querySelector('.sidebar').classList.add('active');
     }
 
-    alert("Not Defterim v21 - Sıralama Menüsü Gizlendi! 🕵️");
+    alert("Not Defterim v22 - Ekran Genişliği Sabitlendi! 📏");
 });
 
 // --- Data Listeners ---
